a1892d8bb4f67244c61743740e170eebf608dcd0
Viviane Ferrari <viviane.tferrari@gmail.com>
2025-06-13 14:53:45 -0300

    Add and creats files on md16-aula6

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,21 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
+const obfuscate = require('gulp-obfuscate');
+const imagemin = require('gulp-imagemin');
+
+function comprimeImagens() {
+    return gulp.src('./source/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build/images'));
+}
+
+function comprimeJavaScript() {
+    return gulp.src('./source/scripts/*.js')
+    .pipe(uglify())
+    .pipe(obfuscate())
+    .pipe(gulp.dest('./build/scripts'));
+}
 
 function compilaSass() {
     return gulp.src('./source/styles/main.scss')
@@ -9,25 +25,8 @@ function compilaSass() {
         .pipe(gulp.dest('./build/styles'));
 }
 
-function funcaoPadrao(callback) {
-    setTimeout(function () {
-        console.log("Executando via Gulp");
-        callback();
-    }, 2000);
+exports.default = function() {
+    gulp.watch('./source/styles/*.scss', {ignoreInitial: false}, gulp.series(compilaSass));
+    gulp.watch('./source/scripts/*.js', {ignoreInitial: false}, gulp.series(comprimeJavaScript));
+    gulp.watch('./source/images/*', {ignoreInitial: false}, gulp.series(comprimeImagens));
 }
-
-function dizOi(callback) {
-    setTimeout(function () {
-        console.log("Olá Gulp");
-        dizTchau();
-        callback();
-    }, 1000);
-}
-
-function dizTchau() {
-    console.log("Tchau Gulp");
-}
-
-exports.default = gulp.parallel(funcaoPadrao, dizOi);
-exports.dizOi = dizOi;
-exports.sass = compilaSass;
--- source/scripts/aritmetica.js
+++ source/scripts/aritmetica.js
@@ -1,3 +1,3 @@
-const somar = require('./sum');
+const somar = require('../../sum');
 
 console.log('Somar:', somar(10,20));
